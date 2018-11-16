8adf5b172ba2efb9c29696acbc57ab9eba71b0bc
Michael Stearn <michaelstearn@michaels-mbp.lan>
2018-11-16 14:06:51 -0500

    not ignoring build files

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/michaelstearn/DEV/gatsby-starter-blog/node_modules/gatsby-plugin-google-analytics/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/michaelstearn/DEV/gatsby-starter-blog/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/Users/michaelstearn/DEV/gatsby-starter-blog/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
+  "component---src-templates-blog-post-js": () => import("/Users/michaelstearn/DEV/gatsby-starter-blog/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
+  "component---src-pages-404-js": () => import("/Users/michaelstearn/DEV/gatsby-starter-blog/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/michaelstearn/DEV/gatsby-starter-blog/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import("/Users/michaelstearn/DEV/gatsby-starter-blog/.cache/data.json")
+
